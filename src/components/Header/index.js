a840bb7c0c70f17a56b6394d5559165d31955551
Aditya Sinha <adityasinha361@gmail.com>
2023-03-01 14:33:07 +0530

    feat: add header

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faFilter } from '@fortawesome/free-solid-svg-icons';
+import './style.css';
+const Header = () => {
+  return (
+    <>
+      <div className='header'>
+        <p>EVENTIFY</p>
+      </div>
+      {/* <FontAwesomeIcon icon={faFilter} size="2x" /> */}
+    </>
+  );
+};
+
+export default Header;
