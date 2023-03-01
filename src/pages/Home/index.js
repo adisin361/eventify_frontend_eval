0bb45ed86a08dac70a1ba2e4ae325d04871772b9
Aditya Sinha <adityasinha361@gmail.com>
2023-03-01 16:46:13 +0530

    feat: add different pages

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import Header from '../../components/Header';
+import React from 'react';
+import ListView from '../../components/ListView';
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <ListView />
+    </>
+  );
+};
+
+export default Home;
---
+++ src/pages/SingleEventView/index.js
@@ -0,0 +1,13 @@
+import Header from '../../components/Header';
+import React from 'react';
+import SingleCardView from '../../components/SingleCardView';
+const SinglePageView = () => {
+  return (
+    <>
+      <Header />
+      <SingleCardView />
+    </>
+  );
+};
+
+export default SinglePageView;
